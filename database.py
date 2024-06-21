6ec75a3b24163911980596e022f24528328f2cec
mishatar <mishatar2004@gmail.com>
2024-06-21 12:02:57 +0300

    add class Connection

--- database.py
+++ database.py
@@ -1,22 +1,48 @@
 from typing import AsyncGenerator
-
+from contextlib import asynccontextmanager
 from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-from settings import db_config
+from settings import BaseSQLConfig, app_config
 
-DATABASE_URL = f"mysql+aiomysql://{db_config.get('user')}:{db_config.get('password')}@{db_config.get('host')}:{db_config.get('port')}/{db_config.get('database')}"
 Base = declarative_base()
 
 metadata = MetaData()
 
-engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+class Connection:
+    def __init__(self):
+        self._engine = create_async_engine(
+            self.__prepare_connection_data(config=app_config.db_config),
+            hide_parameters=False,
+            poolclass=NullPool,
+            future=True,
+            echo=app_config.DEVELOPMENT
+        )
+        self._session = async_sessionmaker(
+            bind=self._engine,
+            expire_on_commit=False,
+            class_=AsyncSession
+        )
 
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
+    @asynccontextmanager
+    async def get_async_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self._session() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception as err:
+                await session.rollback()
+                raise err
+            finally:
+                await session.close()
+
+    @staticmethod
+    def __prepare_connection_data(config: BaseSQLConfig) -> str:
+        """
+        Base hidden prepare connection type.
+        """
+
+        return f"{config.connector}://{config.user}:{config.password}@{config.host}:{config.port}/{config.schema}"
