67c6db1706870b4266ce790d8b16781392501853
mishatar <mishatar2004@gmail.com>
2024-06-21 10:09:42 +0300

    add database config

---
+++ database.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+from settings import db_config
+
+DATABASE_URL = f"mysql+aiomysql://{db_config.get('user')}:{db_config.get('password')}@{db_config.get('host')}:{db_config.get('port')}/{db_config.get('database')}"
+Base = declarative_base()
+
+metadata = MetaData()
+
+engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
